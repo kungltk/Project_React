5d66c60930cb35ee69785c21cddbaed1c66b8b07
kungltk <teerayut.lt@gmail.com>
2021-02-01 23:21:08 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@ import React from 'react';
 import Posts from './Posts';
 import Profile from './Profile';
 import Home from './Home';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, NavLink } from 'react-router-dom';
 const App = () => {
   return (
     <BrowserRouter>
       <div>
-          <Route path="/posts" component={Posts} />
-          <Route path="/profile" componemt={Profile} />
-          <Route path="/home" componemt={Home} />
-          <Route path="/" exact={true} component={Home} />
+        <ul>
+          <li><NavLink to="/">Home</NavLink></li>
+          <li><NavLink to="/posts" activeStyle={{color:"green"}}>All Posts</NavLink></li>
+          <li><NavLink to="/profile" activeClassName="my-active">User Profile</NavLink></li>
+        </ul>
+        <Route path="/posts" component={Posts} />
+        <Route path="/profile" component={Profile} />
+        <Route path="/home" component={Home} />
+        <Route path="/" exact={true} component={Home} />
       </div>
     </BrowserRouter>
   )
